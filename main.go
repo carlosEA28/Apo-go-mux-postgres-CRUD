477bbc4ec50aa0d389ddf89a512b3837da577f4c
carlosEA28 <carloseaduarte28@gmail.com>
2024-07-22 11:45:58 -0300

    add env MAIN_PORT

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/carlos/go-mux-postgres/db"
 	"github.com/carlos/go-mux-postgres/models"
 	"github.com/carlos/go-mux-postgres/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	//inicia as variaveis ambiente
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	db.DBConnection()                //inicia o banco de dados
 	db.DB.AutoMigrate(models.Task{}) //gera as migrações para a criação das tabelas tarefas
@@ -33,5 +41,11 @@ func main() {
 	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
 	r.HandleFunc("/task/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 
-	http.ListenAndServe(":3000", r)
+	port := os.Getenv("MAIN_PORT")
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	http.ListenAndServe(port, r)
 }
